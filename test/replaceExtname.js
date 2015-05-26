e168d7e14193c078b754a4e5238f03cca01c6294
zoubin <zoubin04@gmail.com>
2015-05-26 14:03:46 +0800

    add test for replaceExtname

--- test/replaceExtname.js
+++ test/replaceExtname.js
@@ -7,5 +7,6 @@ test('path.replaceExtname', function (t) {
     t.equal(replace('/a/b/c', '.css'), '/a/b/c.css');
     t.equal(replace('/a/b/c.scss', ''), '/a/b/c');
     t.equal(replace('/a/b/c', ''), '/a/b/c');
+    t.equal(replace('/a/b/c.scss'), '/a/b/c');
     t.end();
 });
